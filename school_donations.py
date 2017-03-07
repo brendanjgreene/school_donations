22d56c50cb319bc7c90244c322c954401b995878
Brendan Greene <brendanjgreene@gmail.com>
2017-03-07 23:46:43 +0000

    modified uri and db name

--- school_donations.py
+++ school_donations.py
@@ -6,8 +6,8 @@ import os
 
 app = Flask(__name__)
 
-MONGO_URI = os.getenv('mongodb://root:kZc-TT4-YcG-ZZR@ds119250.mlab.com:19250', 'mongodb://localhost:27017')
-DBS_NAME = os.getenv('donorsUSA', 'donorsUSA')
+MONGO_URI = os.getenv('MONGODB_URI', 'mongodb://localhost:27017')
+DBS_NAME = os.getenv('MONGO_DB_NAME', 'donorsUSA')
 COLLECTION_NAME = 'projects'
 
 
